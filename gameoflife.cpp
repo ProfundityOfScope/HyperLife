6ebd91c1c5dcab90856323fafbd6287f5433eeda
Seth <bruzewskis@gmail.com>
2021-05-10 15:38:58 -0600

    Can now draw a 2d board

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -1,8 +1,25 @@
 // Game of Life testing
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+template< typename T, size_t N, size_t M >
+void printArray( T(&theArray)[N][M]  ) {
+    for ( int x = 0; x < N; x ++ ) {
+    	cout << ((x==0) ? "[[" : " [");
+        for ( int y = 0; y < M; y++ ) {
+            cout << " " << ((theArray[x][y]) ? "T" : "F") << " ";
+        }
+        cout << ((x==N-1) ? "]]" : "]") << endl;
+    }
+}
 
 int main() {
-    std::cout << "Hello World!\n";
+    cout << "Hey look it's a board" << endl;
+
+    const int board_size = 10;
+    bool array[board_size][board_size] = {0};
+    printArray(array); 
     return 0;
 }
